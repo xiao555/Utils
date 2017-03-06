e7369fb084fcb27378a30f716d27d686f02accfb
xiao555 <zhangruiwu32@gmail.com>
2017-03-06 10:17:15 +0800

    update 2017-03-06

--- push.sh
+++ push.sh
@@ -1,10 +1,10 @@
-echo 'push starting' $(date '+%Y-%m-%d +%H:%M:%S')
+echo 'push starting' $(date '+%Y-%m-%d')
 git add .
-git commit -m "update"
+git commit -m "update $(date '+%Y-%m-%d')"
 git push -f origin master
 git checkout -B gh-pages
 git add .
-git commit -m "update"
+git commit -m "update $(date '+%Y-%m-%d')"
 git push -f origin gh-pages
 git checkout master
 echo 'push'
